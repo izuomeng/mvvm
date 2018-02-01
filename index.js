999a06cb82d0962b1420255d64919523fddc4ca8
izuomeng <dmate@foxmail.com>
2018-02-01 22:10:03 +0800

    compile  {{ mustache }}

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-import observe from './lib/observer'
+import MVVM from './lib/mvvm'
 
-class MVVM {
-  constructor(options) {
-    this._data = observe(options.data)
-    
+window.MVVM = MVVM
+/* const vm = new MVVM({
+  el: '#root',
+  data: {
+    name: 'zuomeng',
+    age: 21
   }
+})
+
+function in1(e) {
+  vm.name = e.target.value
 }
+function in2(e) {
+  vm.age = e.target.value
+} */
