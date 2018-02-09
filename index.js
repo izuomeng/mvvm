98a1e71a9eb725009e805ab172590576327b2b6f
izuomeng <dmate@foxmail.com>
2018-02-09 14:23:03 +0800

    test

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 import MVVM from './lib/mvvm'
 
-window.MVVM = MVVM
-/* const vm = new MVVM({
+const vm = new MVVM({
   el: '#root',
   data: {
-    name: 'zuomeng',
-    age: 21
+    person: {
+      name: {
+        first: 'Tom',
+        last: 'Smith'
+      },
+      age: 15
+    }
   }
 })
-
-function in1(e) {
-  vm.name = e.target.value
-}
-function in2(e) {
-  vm.age = e.target.value
-} */
